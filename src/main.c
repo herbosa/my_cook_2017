c8c1b0bcbd173705e4f9f074d6c5f0946c4afaf1
Antoine HERBOSA <aherbosa@localhost.localdomain>
2018-02-12 17:17:17 +0100

    add buton on pause menu

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ sfRenderWindow *renderwindow_create(sfRenderWindow *wd)
 
 	v_mode.width = 1920;
 	v_mode.height = 1075;
+	v_mode.bitsPerPixel = 32;
 	wd = sfRenderWindow_create(v_mode, "MY_COOK", sfDefaultStyle, NULL);
 	return (wd);
 }
@@ -41,27 +42,70 @@ void display_home(sfRenderWindow *window, sprite_t **bg)
 	sfRenderWindow_display(window);
 }
 
-void touch_pause(sfRenderWindow *window, sprite_t **bg)
+void pause_setsprite(sprite_t **brk)
+{
+	sfSprite_setTextureRect(brk[0]->s_sprt, brk[0]->r_sprt);
+	sfSprite_setTextureRect(brk[1]->s_sprt, brk[1]->r_sprt);
+}
+
+void touch_pause(sfRenderWindow *window, sprite_t **bg, sprite_t **brk)
 {
 	if (sfKeyboard_isKeyPressed(sfKeyEscape))
 		bg[0]->o_sprt = 1;
 	if (sfKeyboard_isKeyPressed(sfKeyQ))
-		bg[0]->o_sprt = 0;;
+		sfRenderWindow_close(window);
 }
 
-void display_pause(sfRenderWindow *window, sprite_t **bg)
+void button_pause(sprite_t **brk, sfEvent event)
+{
+	if (event.mouseMove.x  > brk[0]->v_sprt.x &&
+		event.mouseMove.x < brk[0]->v_sprt.x + 200 &&
+		event.mouseMove.y > brk[0]->v_sprt.y &&
+		event.mouseMove.y < brk[0]->v_sprt.y + 200)
+		brk[0]->r_sprt.top = 200;
+	else
+		brk[0]->r_sprt.top = 0;
+	if (event.mouseMove.x  > brk[1]->v_sprt.x &&
+		event.mouseMove.x < brk[1]->v_sprt.x + 200 &&
+		event.mouseMove.y > brk[1]->v_sprt.y &&
+		event.mouseMove.y < brk[1]->v_sprt.y + 200)
+		brk[1]->r_sprt.top = 200;
+	else
+		brk[1]->r_sprt.top = 0;
+}
+
+void clicked_pause(sfRenderWindow *window, sprite_t **bg, sprite_t **brk,
+			sfEvent event)
+{
+	if (event.mouseButton.x > brk[0]->v_sprt.x &&
+		event.mouseButton.x < brk[0]->v_sprt.x + 200 &&
+		event.mouseButton.y > brk[0]->v_sprt.y &&
+		event.mouseButton.y < brk[0]->v_sprt.y + 200)
+		sfRenderWindow_close(window);
+	if (event.mouseButton.x > brk[1]->v_sprt.x &&
+		event.mouseButton.x < brk[1]->v_sprt.x + 200 &&
+		event.mouseButton.y > brk[1]->v_sprt.y &&
+		event.mouseButton.y < brk[1]->v_sprt.y + 200)
+		bg[0]->o_sprt = 1;
+}
+
+void display_pause(sfRenderWindow *window, sprite_t **bg, sprite_t **brk)
 {
 	sfEvent event;
 
 	while (sfRenderWindow_pollEvent(window, &event)) {
-		if (event.type == sfEvtClosed) {
+		if (event.type == sfEvtClosed)
 			sfRenderWindow_close(window);
-		}
-		if (event.type == sfEvtKeyPressed) {
-			touch_pause(window, bg);
-		}
+		if (event.type == sfEvtKeyPressed)
+			touch_pause(window, bg, brk);
+		button_pause(brk, event);
+		if (event.type == sfEvtMouseButtonPressed)
+			clicked_pause(window, bg, brk, event);
 	}
+	pause_setsprite(brk);
 	sfRenderWindow_drawSprite(window, bg[1]->s_sprt, NULL);
+	sfRenderWindow_drawSprite(window, brk[0]->s_sprt, NULL);
+	sfRenderWindow_drawSprite(window, brk[1]->s_sprt, NULL);
 	sfRenderWindow_display(window);
 }
 
@@ -70,7 +114,7 @@ void touch_game(sfRenderWindow *window, sprite_t **bg)
 	if (sfKeyboard_isKeyPressed(sfKeyEscape))
 		bg[0]->o_sprt = 2;
 	if (sfKeyboard_isKeyPressed(sfKeyQ))
-		bg[0]->o_sprt = 0;;
+		sfRenderWindow_close(window);
 }
 
 void display_game(sfRenderWindow *window, sprite_t **bg)
@@ -90,7 +134,7 @@ void display_game(sfRenderWindow *window, sprite_t **bg)
 	sfRenderWindow_display(window);
 }
 
-void game_loop(sfRenderWindow *window, sprite_t **bg)
+void game_loop(sfRenderWindow *window, sprite_t **bg, sprite_t **brk)
 {
 	while (sfRenderWindow_isOpen(window)) {
 		if (bg[0]->o_sprt == 0)
@@ -98,8 +142,34 @@ void game_loop(sfRenderWindow *window, sprite_t **bg)
 		if (bg[0]->o_sprt == 1)
 			display_game(window, bg);
 		if (bg[0]->o_sprt == 2)
-			display_pause(window, bg);
+			display_pause(window, bg, brk);
 	}
+}
+
+sfIntRect create_rect(sfIntRect rect)
+{
+	rect.top = 0;
+	rect.left = 0;
+	rect.width = 200;
+	rect.height = 200;
+	return (rect);
+}
+
+sprite_t **fill_brk(sprite_t **brk)
+{
+	brk[0] = malloc(sizeof(sprite_t) * 1);
+	brk[0] = create_sprite(brk[0], "src/pictures/quitb.png");
+	brk[0]->v_sprt.x = 600;
+	brk[0]->v_sprt.y = 700;
+	brk[0]->r_sprt = create_rect(brk[0]->r_sprt);
+	sfSprite_setPosition(brk[0]->s_sprt,  brk[0]->v_sprt);
+	brk[1] = malloc(sizeof(sprite_t) * 1);
+	brk[1] = create_sprite(brk[1], "src/pictures/reume_b.png");
+	brk[1]->v_sprt.x = 1000;
+	brk[1]->v_sprt.y = 700;
+	brk[1]->r_sprt = create_rect(brk[1]->r_sprt);
+	sfSprite_setPosition(brk[1]->s_sprt,  brk[1]->v_sprt);
+	return (brk);
 }
 
 sprite_t **fill_bg(sprite_t **bg)
@@ -118,12 +188,16 @@ sprite_t **fill_bg(sprite_t **bg)
 
 int main(void)
 {
-	sfRenderWindow *window = NULL;
-	sprite_t **bg = malloc(sizeof(sprite_t *) * 1);
+	sfRenderWindow *window = malloc(sizeof(sfRenderWindow *) * 1);
+	sprite_t **bg = malloc(sizeof(sprite_t *) * 4);
+	sprite_t **brk = malloc(sizeof(sprite_t *) * 2);
 
+	if (bg == NULL || brk == NULL)
+		return (84);
 	bg = fill_bg(bg);
+	brk = fill_brk(brk);
 	window = renderwindow_create(window);
 	sfRenderWindow_setFramerateLimit(window, 60);
-	game_loop(window, bg);
-	sfRenderWindow_destroy(window);
+	game_loop(window, bg, brk);
+	return (0);
 }
