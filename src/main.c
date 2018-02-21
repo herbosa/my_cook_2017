b6cb993e7629346d1eeee7a5767008c7f439f0d8
Hugo TRINH QUY <hquy@localhost.localdomain>
2018-02-21 11:46:46 +0100

    norminette helper

--- src/main.c
+++ src/main.c
@@ -108,28 +108,32 @@ void touch_help(sfRenderWindow *window, sprite_t **bg)
 		bg[0]->o_sprt = 0;
 }
 
+void change_sprt(sfEvent event, sprite_t **bg, sfRenderWindow *window)
+{
+	if (event.mouseButton.x  > bg[4]->v_sprt.x &&
+		event.mouseButton.x < bg[4]->v_sprt.x + 100 &&
+		event.mouseButton.y > bg[4]->v_sprt.y &&
+		event.mouseButton.y < bg[4]->v_sprt.y + 100)
+		sfRenderWindow_close(window);
+	if (event.mouseMove.x  > bg[4]->v_sprt.x &&
+		event.mouseMove.x < bg[4]->v_sprt.x + 100 &&
+		event.mouseMove.y > bg[4]->v_sprt.y &&
+		event.mouseMove.y < bg[4]->v_sprt.y + 100)
+		bg[4]->r_sprt.top = 100;
+	else
+		bg[4]->r_sprt.top = 0;
+	if (event.mouseButton.x > 824 && event.mouseButton.x < 824 + 520 &&
+		event.mouseButton.y > 303 && event.mouseButton.y < 303 + 390)
+		bg[0]->o_sprt = 1;
+	else if (event.mouseButton.x > 30 && event.mouseButton.x < 270 &&
+		event.mouseButton.y > 550 && event.mouseButton.y < 800)
+		bg[0]->o_sprt = 3;
+}
+
 void clicked_home(sfEvent event, sprite_t **bg, sfRenderWindow *window)
 {
-	if (event.type == sfEvtMouseButtonPressed) {
-		if (event.mouseButton.x  > bg[4]->v_sprt.x &&
-			event.mouseButton.x < bg[4]->v_sprt.x + 100 &&
-			event.mouseButton.y > bg[4]->v_sprt.y &&
-			event.mouseButton.y < bg[4]->v_sprt.y + 100)
-			sfRenderWindow_close(window);
-		if (event.mouseMove.x  > bg[4]->v_sprt.x &&
-			event.mouseMove.x < bg[4]->v_sprt.x + 100 &&
-			event.mouseMove.y > bg[4]->v_sprt.y &&
-			event.mouseMove.y < bg[4]->v_sprt.y + 100)
-			bg[4]->r_sprt.top = 100;
-		else
-			bg[4]->r_sprt.top = 0;
-		if (event.mouseButton.x > 824 && event.mouseButton.x < 824 + 520 &&
-			event.mouseButton.y > 303 && event.mouseButton.y < 303 + 390)
-			bg[0]->o_sprt = 1;
-		else if (event.mouseButton.x > 30 && event.mouseButton.x < 270 &&
-			event.mouseButton.y > 550 && event.mouseButton.y < 800)
-			bg[0]->o_sprt = 3;
-	}
+	if (event.type == sfEvtMouseButtonPressed)
+		change_sprt(event, bg, window);
 	sfSprite_setTextureRect(bg[4]->s_sprt, bg[4]->r_sprt);
 }
 
